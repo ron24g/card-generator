2090d9ba4f2a61f2355c7a5ff22c85e1e6db9696
ron24g <ron24g@yahoo.com>
2022-02-10 04:30:10 +0000

    need to get random color

--- src/app.js
+++ src/app.js
@@ -7,5 +7,28 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let top = document.querySelector(".top");
+  let mid = document.querySelector(".mid");
+  let bot = document.querySelector(".bot");
+  let cardSymbol = ["♦", "♠", "♣", "♥"];
+  let cardNumber = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let targetSymbol = Math.floor(Math.random() * cardSymbol.length);
+  let targetNumber = Math.floor(Math.random() * cardNumber.length);
+  top.innerHTML = `${cardSymbol[targetSymbol]}`;
+  mid.innerHTML = `${cardNumber[targetNumber]}`;
+  bot.innerHTML = `${cardSymbol[targetSymbol]}`;
 };
